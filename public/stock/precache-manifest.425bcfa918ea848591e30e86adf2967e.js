d38dd8be8c47d171aa1a25abd7ce20997f806a34
liushuai <313628216@qq.com>
2020-01-10 21:58:22 +0800

    add some static files

---
+++ public/stock/precache-manifest.425bcfa918ea848591e30e86adf2967e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeaba35ccc1f5b1d80110eb92936549f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66629d354f38947b5828",
+    "url": "/static/css/main.1c36c410.chunk.css"
+  },
+  {
+    "revision": "343066c37dbe18578bf8",
+    "url": "/static/js/2.4c26e658.chunk.js"
+  },
+  {
+    "revision": "66629d354f38947b5828",
+    "url": "/static/js/main.f7a8aa12.chunk.js"
+  },
+  {
+    "revision": "71382eb663e06a3ffb4a",
+    "url": "/static/js/runtime-main.80425956.js"
+  }
+]);
