ccc359d6cb3080ca3360a7fee38487995cb775af
liushuai <313628216@qq.com>
2020-01-10 20:12:25 +0800

    add some static files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const app = express();
-const port = 80;
+const port = 443;
 
 const options = {
   key: fs.readFileSync("/etc/letsencrypt/live/vuee.me/privkey.pem"),
